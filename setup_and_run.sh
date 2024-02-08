50e1cdb9223b633a4adb3018747541c2ef02c5b0
Drashti1603 <20it017@charusat.edu.in>
2024-02-08 12:15:25 +0530

    Fourth commit

---
+++ setup_and_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+chmod +x setup_and_run.sh
+
+# Print current working directory
+pwd
+
+# Install Robot Framework and Robot Framework AppiumLibrary
+python3 -m pip install robotframework
+python3 -m pip install robotframework-appiumlibrary
+
+# Print Python version and installed packages
+python3 --version
+pip3 list
+
+# Run the Robot Framework test suite specified in the requirements.txt file
+python3 -m robot.run $(cat requirements.txt)
